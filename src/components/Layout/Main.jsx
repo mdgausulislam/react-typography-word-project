19fcddbc5441327a7312e5854c6b814d5d10f2b1
unknown <gausulislam5@gmail.com>
2023-10-16 11:05:20 +0600

    update react router setup and tailwind setup for this project

---
+++ src/components/Layout/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className='container grid grid-cols-2 gap-96'>
+            <div>
+                <h1>Typography</h1>
+
+            </div>
+            <div>
+                <h1>Text</h1>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Main;
